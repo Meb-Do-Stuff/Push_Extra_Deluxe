5319a473ba5054e7f324d1e9e132f92f6a7d1540
Meb do stuff <69876322+Meb-Do-Stuff@users.noreply.github.com>
2024-08-18 17:52:32 +0200

    Based from https://github.com/gluon/AbletonLive12_MIDIRemoteScripts/tree/main/Push Fixed everything that wasn't working (decompilation didn't owrked well)

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, print_function, unicode_literals
+from ableton.v2.control_surface.capabilities import AUTO_LOAD_KEY, CONTROLLER_ID_KEY, FIRMWARE_KEY, HIDDEN, NOTES_CC, PORTS_KEY, SCRIPT, SYNC, TYPE_KEY, controller_id, inport, outport
+from .firmware_handling import get_provided_firmware_version
+from .push import Push
+
+def get_capabilities():
+    return {CONTROLLER_ID_KEY: (controller_id(vendor_id=2536,
+                          product_ids=[21],
+                          model_name="Ableton Push")), 
+     
+     PORTS_KEY: [
+                 inport(props=[HIDDEN, NOTES_CC, SCRIPT]),
+                 inport(props=[]),
+                 outport(props=[HIDDEN, NOTES_CC, SYNC, SCRIPT]),
+                 outport(props=[])], 
+     
+     TYPE_KEY: "push", 
+     FIRMWARE_KEY: (get_provided_firmware_version()), 
+     AUTO_LOAD_KEY: True}
+
+
+def create_instance(c_instance):
+    return Push(c_instance=c_instance)
+    ...
---
+++ special_instrument_component.py
@@ -0,0 +1,6 @@
+﻿from pushbase.instrument_component import NoteLayout
+
+
+class SpecialNoteLayout(NoteLayout):
+    def __init__(self, *a, **k):
+        super(SpecialNoteLayout, self).__init__(*a, **k)
